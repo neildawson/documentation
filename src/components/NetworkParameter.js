b3f0db175bc662620ed9461c9f9b5df4c99f1c75
Edd <edd@vega.xyz>
2022-07-15 14:31:01 +0100

    Write network params to json at build time so there can be a default

--- src/components/NetworkParameter.js
+++ src/components/NetworkParameter.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import useIsBrowser from '@docusaurus/useIsBrowser';
+// JSON fetched at build time is used as a default
+import mainnetNetworkParameters from '../../data/mainnet_network_parameters.json';
+import testnetNetworkParameters from '../../data/testnet_network_parameters.json';
 
-const networkApiUrl = {
+const networkApiUrl =
+{
   'TESTNET': 'https://lb.testnet.vega.xyz/network/parameters',
   'MAINNET': 'https://api.token.vega.xyz/network/parameters',
 }
@@ -10,13 +15,26 @@ const explorerUrl = {
   'MAINNET': 'https://explorer.vega.xyz/network-parameters',
 }
 
-function restructureData(apiResponse) {
+export function restructureData(apiResponse) {
   const dict = {}
   apiResponse.forEach(a => {
     dict[a['key']] = a['value']
   })
-  return dict
+  return dict;
 }
+
+/**
+ * Use the build time network variables. Overwritten by a network fetch
+ * to ensure it's up to date for the current time
+ */
+export function getDefaults(networks) {
+  if (networks === 'TESTNET') {
+    return restructureData(testnetNetworkParameters.networkParameters);
+  } else {
+    return restructureData(mainnetNetworkParameters.networkParameters);
+  }
+}
+
 
 export default function NetworkParameter(props) {
   const vega_network = props?.frontMatter?.vega_network || 'TESTNET';
@@ -27,7 +45,7 @@ export default function NetworkParameter(props) {
     throw new Error("Missing vega_network");
   }
 
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(getDefaults(vega_network));
 
   useEffect(() => {
     const fetchData = async () => {
@@ -41,9 +59,9 @@ export default function NetworkParameter(props) {
 
   if (data) {
     const value = data[props.param]
-    const displayValue = suffix ? <>{value} {suffix}< /> : value;
+    const displayValue = suffix ? <strong>{value} {suffix}</strong> : value;
 
-    return (<a href={explorerUrl[vega_network]} className="networkparameter" title={`Set by network parameter: ${props.param}`} >
+    return (<a href={explorerUrl[vega_network]} className="networkparameter" title={`Set by network parameter: ${props.param}`}>
       <span className="networkparametericon">🛠️</span>
       {(hideName ? null : <span className="networkparametername">{props.param}: </span>)}
       <span className="networkparametervalue">{displayValue || `Could not find ${props.param}`}</span>
